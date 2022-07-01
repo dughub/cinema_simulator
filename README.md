df8988667c18340eb37bfb10da9135ffaf4e9ef6
dughub <dughub@outlook.com>
2022-07-01 17:46:59 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cinema Simulator
+
+This repo simulates waiting times in cinema theatres, and aims to answer the question:
+>"What is the optimimum number of employees given some variables." 
+
+
+*Based on the realpython.com tutorial 
+['simpy simulating with python'](https://realpython.com/simpy-simulating-with-python/#creating-the-environment-class-definition)*
---
+++ main.py
@@ -0,0 +1,20 @@
+"""Running the simulation in the cinema environment."""
+
+import simpy
+import statistics
+import random
+import logging
+
+
+class Cinema:
+    def __init__(self, env):
+        self.env = env
+        self.cashiers = simpy.Resource(self.env, num_cashiers)
+
+
+def optimise_wait_times():
+    wait_times = []
+
+
+if __name__ == '__main__':
+    optimise_wait_times()
